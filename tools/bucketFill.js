de8d628f9f6382032b603ad647df694fc59ab160
ahmedMshaban <ahmed.ms.elias@gmail.com>
2021-12-21 09:50:12 +0300

    bucketfill displayconfig

--- tools/bucketFill.js
+++ tools/bucketFill.js
@@ -1,10 +1,14 @@
 class BucketFill extends Tools {
-    constructor(icon, name) {
-      super(icon, name);
-    }
-  
-    draw() {
-
-    }
+  constructor(icon, name) {
+    super(icon, name);
+    this.color = new Color();
   }
-  
+
+  draw() {}
+
+  displayConfigOptions() {
+    return [
+      this.color.displayFill("bucketfill"),
+    ];
+  }
+}
