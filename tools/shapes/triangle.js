afb20e36df364e6c47740693c0f2f510e349a523
ahmedMshaban <ahmed.ms.elias@gmail.com>
2022-01-21 11:45:23 +0300

    improve triangle class code strcutre

--- tools/shapes/triangle.js
+++ tools/shapes/triangle.js
@@ -3,26 +3,42 @@ class Triangle extends Tools {
     super(icon, name);
     this.color = new Color();
     this.isFilled = new IsFilled();
+    this.angle = 0;
+    this.x1, this.y1, this.x2, this.y2, this.x3, (this.y3 = -1);
   }
 
   draw() {
+
+
+    angleMode(DEGREES);
     if (mouseIsPressed) {
       if (!this.isDrawing) {
         this.isDrawing = true;
         this.isFilled.status ? fill(this.color.fill) : noFill();
         stroke(this.color.outline);
-        triangle(
-          mouseX - 50,
-          mouseY + 50,
-          mouseX + 50,
-          mouseY + 50,
-          mouseX,
-          mouseY - 50
-        );
+        angleMode(DEGREES);
+        // translate(500, 500);
+        rotate(this.angle);
+        // triangle(0, 0, 100, 0, 50, -80); //Triangle x1, y1
+        this.x1 = mouseX - 50;
+        this.y1 = mouseY + 50;
+        this.x2 = mouseX + 50;
+        this.y2 = mouseY + 50;
+        this.x3 = mouseX;
+        this.y3 = mouseY - 50;
+        triangle(this.x1, this.y1, this.x2, this.y2, this.x3, this.y3);
+        console.log(this.x1, this.y1, this.x2, this.y2, this.x3, this.y3);
       }
     } else {
       this.isDrawing = false;
     }
+
+    if (this.isDrawing) {
+    }
+  }
+
+  show() {
+    
   }
 
   displayConfigOptions() {
